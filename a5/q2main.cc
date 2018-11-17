0e5226a93f66f68c09852f6ee47422e211437908
timbaek <tim.baek@hotmail.com>
2018-11-17 17:59:39 -0500

    A5: fix input handling in main

--- a5/q2main.cc
+++ a5/q2main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "MPRNG.h"
 #include "q2voter.h"
 #include "q2printer.h"
@@ -16,6 +17,12 @@ void usage( char *argv[] ) {
     exit( EXIT_FAILURE );				// TERMINATE
 } // usage
 
+bool isPureInteger( string s ) {
+    string::const_iterator it = s.begin();
+    while ( it != s.end() && isdigit(*it) ) ++it;
+    return !s.empty() && it == s.end();
+}
+
 MPRNG mprng;
     
 int main( int argc, char * argv[] ) {
@@ -24,19 +31,19 @@ int main( int argc, char * argv[] ) {
     try {
         switch ( argc ) {
             case 6: if ( strcmp( argv[5], "d" ) != 0 ) {  // default ?
-                processors = stoi( argv[5] ); if ( processors <= 0 ) throw 1;
+                processors = stoi( argv[5] ); if ( processors <= 0 || !isPureInteger( argv[5]) ) throw 1;
             } // if
             case 5: if ( strcmp( argv[4], "d" ) != 0 ) {  // default ?
-                seed = stoi( argv[4] ); if ( seed <= 0 ) throw 1;
+                seed = stoi( argv[4] ); if ( seed <= 0 || !isPureInteger( argv[4]) ) throw 1;
             } // if
             case 4: if ( strcmp( argv[3], "d" ) != 0 ) {  // default ?
-                votes = stoi( argv[3] ); if ( votes <= 0 ) throw 1;
+                votes = stoi( argv[3] ); if ( votes <= 0 || !isPureInteger( argv[3]) ) throw 1;
             } // if
             case 3: if ( strcmp( argv[2], "d" ) != 0 ) {  // default ?
-                group = stoi( argv[2] ); if ( group <= 0 ) throw 1;
+                group = stoi( argv[2] ); if ( group <= 0 || !isPureInteger( argv[2]) ) throw 1;
             } // if
             case 2: if ( strcmp( argv[1], "d" ) != 0 ) {  // default ?
-                voters = stoi( argv[1] ); if ( voters <= 0 ) throw 1;
+                voters = stoi( argv[1] ); if ( voters <= 0 || !isPureInteger( argv[1]) ) throw 1;
             } // if
             case 1: break;                                // use all defaults
             default: throw 1;
